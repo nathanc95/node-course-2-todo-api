9c282f71aa238ba9889ea13b9ccd8534eb999bc0
nathan <nathanchemla95@gmail.com>
2017-10-04 12:11:49 +0300

    add server with query

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -3,29 +3,32 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo}=require('./../server/models/todo');
 const {Users}=require('./../server/models/user');
 
-// var id="59d4843e2f06049ca53bacb8";
-// if(!ObjectId.isValid(id)){
-//   console.log('Id not valid');
-// }
-//
-// Todo.find({
-//   _id:id
-// }).then((todos)=>{
-//   console.log('todos',todos);
-// });
-//
-// Todo.findOne({
-//   _id:id
-// }).then((todo)=>{
-//   console.log('todos',todo);
-// });
-//
-// Todo.findById(id).then((todo)=>{
-//   if(!todo){
-//     return console.log('id not found');
-//   }
-//   console.log('todo by id',todo);
-// }).catch((e)=>console.log(e));
+var id="59d4843e2f06049ca53bacb8";
+if(!ObjectId.isValid(id)){
+  console.log('Id not valid');
+}
+
+Todo.find({
+  _id:id
+}).then((todos)=>{
+  console.log('todos',todos);
+});
+
+Todo.findOne({
+  _id:id
+}).then((todo)=>{
+  console.log('todos',todo);
+});
+
+Todo.findById(id).then((todo)=>{
+  if(!todo){
+    return console.log('id not found');
+  }
+  console.log('todo by id',todo);
+}).catch((e)=>console.log(e));
+
+
+
 
 var id="59d4866a5a84bfbccc4547b5";
 Users.find({
