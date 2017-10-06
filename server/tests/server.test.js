8df558b782ee3b8e37e768511b753c2c7c2c9ff7
nathan <nathanchemla95@gmail.com>
2017-10-06 13:22:47 +0300

    changing on server

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -104,3 +104,43 @@ describe('POST/todos',()=>{
           .end(done);
     });
   });
+describe('DELETE /todos/:id',()=>{
+  it('should remove a todo',(done)=>{
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todo._id).toBe(hexId);
+    })
+    .end((err,res)=>{
+      if(err){
+        return done(err);
+      }
+
+      Todo.findById(hexId).then((todo)=>{
+        expect(todo).toNotExist();
+        done();
+      }).catch((e)=>{
+        done(e)
+      });
+    })
+  });
+
+  it('should return 404 if todo not found',(done)=>{
+    var hexString=new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexString}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object is invalid',(done)=>{
+    request(app)
+      .delte(`/todos/123abc`)
+      .expect(404)
+      .end(done);
+  });
+});
